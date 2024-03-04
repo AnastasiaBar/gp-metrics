c5a11d1542d8530c6a36723c2e20d5cc5128dbf5
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2024-03-04 15:31:24 +0300

    add: добавление метрики

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const form = document.querySelector('.form');
 form.onsubmit = function(evt) {
     console.log('test')
     evt.preventDefault();
+
+    window['ym'](96649585,'reachGoal','data from the form');
 };
