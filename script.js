609ddb8c843a24aaa999036f8bf8a5df36f7dd99
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2024-03-04 16:07:10 +0300

    add: добавление метрики

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ form.onsubmit = function(evt) {
     evt.preventDefault();
 
     //window['ym'](96649585,'reachGoal','data from the form');
+
+
+
+    $.ajax({
+        type: "GET",
+        url: "https://jsonplaceholder.typicode.com/posts/1",
+        dataType: "html",
+        data: data,
+        success: function (out) {
+            console.log(out)
+        }
+    });
 };
